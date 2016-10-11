aaba7106cf6ba6d6423df05e8469fde8463968fb
U1and0 <e01.ando60@gmail.com>
2016-10-11 16:15:26 +0900

    time_checker.py first commit

---
+++ time_checker.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+import datetime
+from more_itertools import pairwise
+import glob
+import doctest
+import simplejson
+
+with open('parameter.json', 'r') as f:
+    param = simplejson.load(f)
+
+
+def tracefile(dirname):
+    '''
+    ## time_checker ver1.0
+
+    __USAGE__
+    引数:
+        dirname : datetimeがファイル名になったファイルが詰まったディレクトリの場所(string型)
+    戻り値:
+        timepair : datetime2つが入ったタプル(tuple型)
+
+    __INTRODUCTION__
+    あるファイル名から次のファイルが作成された時刻の差分を出す。
+
+    __ACTION__
+    SAtraceGraph.pyのサブプロセスのfilecheck.pyでエラーが生じたときに実行される。
+
+    __UPDATE1.0__
+    First commit
+
+    __TODO__
+    None
+
+    __TEST__
+
+    >>> tracefile(param['out']+'/160817/rawdata/trace/')
+    (datetime.datetime(1900, 1, 1, 8, 50, 29), datetime.datetime(1900, 1, 1, 8, 59, 12))
+
+    '''
+    line = glob.glob1(dirname, '*')
+    ttlist = [datetime.datetime.strptime(i[9:15], '%H%M%S') for i in line]
+    for timepair in pairwise(ttlist):
+        sub = timepair[1] - timepair[0]
+        if not datetime.timedelta(minutes=4) < sub < datetime.timedelta(minutes=6):
+            return timepair  # 時間差の生じたとこ
+
+
+if __name__ == '__main__':
+    doctest.testmod()
